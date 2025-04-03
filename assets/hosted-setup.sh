e9f7799559db6500bec4215101eb3a456540bfab
Gabriel Loyola Daiqui <gabriel.loyola.daiqui@aisec.fraunhofer.de>
2025-04-03 17:17:00 +0200

    Adds GPL header to scripts
    Signed-off-by: Gabriel Loyola Daiqui
    <gabriel.loyola.daiqui@aisec.fraunhofer.de>

--- assets/hosted-setup.sh
+++ assets/hosted-setup.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
+#
+# This file is part of GyroidOS
+# Copyright(c) 2013 - 2025 Fraunhofer AISEC
+# Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms and conditions of the GNU General Public License,
+# version 2 (GPL 2), as published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GPL 2 license for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses/>
+#
+# The full GNU General Public License is included in this distribution in
+# the file called "COPYING".
+#
+# Contact Information:
+# Fraunhofer AISEC <gyroidos@aisec.fraunhofer.de>
+#
+
+set -euo pipefail
 
 print() {
     echo
     echo "$*"
 }
-
-set -euo pipefail
 
 GROUP_NAME="cml-control"
 CERTS_DIR=~/test-certs
